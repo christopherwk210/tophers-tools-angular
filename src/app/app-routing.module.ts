9577f797e6cb925382bde6798a1b99bacb49f690
Chris Anselmo <canselmo@hybridhealth.com>
2020-01-28 17:16:16 -0800

    Updated build to adjust for deployment route, manually writing route config to fix build issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { tools } from './config/tools';
 import { ToolListComponent } from './components';
+
+import {
+  ColorPickerComponent,
+  GmlJavadocGenComponent,
+  JsonToDsComponent,
+  RatiosComponent
+} from './components/tools';
 
 const routes: Routes = [
   {
     path: '',
     component: ToolListComponent
+  },
+  {
+    path: 'gm/color-picker',
+    component: ColorPickerComponent
+  },
+  {
+    path: 'gm/gml-javadoc-gen',
+    component: GmlJavadocGenComponent
+  },
+  {
+    path: 'math/ratiomatic',
+    component: RatiosComponent
   }
 ];
-
-tools.forEach(tool => {
-  routes.push({
-    path: tool.route,
-    component: tool.component
-  });
-});
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
--- src/app/config/tools.ts
+++ src/app/config/tools.ts
@@ -23,14 +23,14 @@ export const tools: Tool[] = [
   {
     title: 'GM Color Picker',
     description: 'The easy way to get GML color codes.',
-    route: 'gm/color-picker',
+    route: '/gm/color-picker',
     component: ColorPickerComponent,
     category: Category.GAMEMAKER
   },
   {
     title: 'GML JavaDoc Gen',
     description: 'Quickly turn GameMaker: Studio script comments into GameMaker Studio 2 compatible JavaDoc comments.',
-    route: 'gm/gml-javadoc-gen',
+    route: '/gm/gml-javadoc-gen',
     component: GmlJavadocGenComponent,
     category: Category.GAMEMAKER
   },
@@ -45,7 +45,7 @@ export const tools: Tool[] = [
   {
     title: 'Ratiomatic',
     description: 'Automatically calculate ratios. Handy for working with resolutions.',
-    route: 'math/ratiomatic',
+    route: '/math/ratiomatic',
     component: RatiosComponent,
     category: Category.MATH
   }
